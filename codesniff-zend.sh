febdadd16a1f537f17bfd8f5a84deb3d6259ab89
Victory <git@dfhu.org>
2014-03-04 19:32:49 -0500

    a lint/codesniffer script for git pre-commit hook

---
+++ codesniff-zend.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# Used in .git/hooks/pre-commit with the following code snipet
+#
+# LINT="$(git rev-parse --show-toplevel)/code-sniffer.sh"
+# $LINT
+# if [ $? -ne 0 ]; then
+#     echo "Not committing do to Lint errors."
+#     exit 1
+# fi
+
+cd $(git rev-parse --show-toplevel)
+
+PHPCS=$(which phpcs)
+STANDARD="Zend"
+
+if [ -z "$PHPCS" ]; then
+    echo "phpcs (CodeSniffer) not installed"
+    exit 1000
+fi
+
+RESULT=0
+while read fn; do
+    $PHPCS --standard=$STANDARD "$fn"
+    RESULT=$(($? + $RESULT))
+    echo $RESULT
+done < <(find . -name "*.php")
+
+exit $RESULT
