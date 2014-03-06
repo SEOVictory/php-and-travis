298e4449fa51db0a5a0ec99d24e5b4f6bd05f045
Victory <git@dfhu.org>
2014-03-05 21:41:42 -0500

    travis run

--- tests/TestMapTrap.php
+++ tests/TestMapTrap.php
@@ -16,4 +16,16 @@ class MapTrapTest extends \PHPUnit_Framework_TestCase
     $actual = $mt($ar, 5);
     $this->assertEquals($expected, $actual);
   }
+
+  public function testHitTrap()
+  {
+    $mt = new MapTrap();
+    $mt->setCallback(function ($vv) {
+        return $vv + 1;
+    });
+    $ar = array(0, 0, 4, 0, 0);
+    $expected = array(1, 1, 5, 5, 5);
+    $actual = $mt($ar, 5);
+    $this->assertEquals($expected, $actual);
+  }
 }
